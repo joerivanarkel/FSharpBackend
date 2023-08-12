abff98492badcbc21fddd92a2ea5bcdfd501cfb0
joerivanarkel <joeri@vanarkel.net>
2023-08-12 14:34:57 +0200

    refactor

--- Api/Controllers/CountryController.fs
+++ Api/Controllers/CountryController.fs
@@ -6,6 +6,8 @@ open joerivanarkel.Data.Repositories
 open joerivanarkel.Api.Models
 open joerivanarkel.Data.Models
 
+exception CountryControllerException of string * exn
+
 [<ApiController>]
 [<Route("[controller]")>]
 type CountryController (logger: ILogger<CountryController>, countryRepository:ICountryRepository) =
@@ -13,28 +15,49 @@ type CountryController (logger: ILogger<CountryController>, countryRepository:IC
 
     [<HttpGet>]
     member _.Get() : Country list =
-        logger.LogInformation("Get all countries")
-        countryRepository.GetCountries()
+        try
+            logger.LogInformation("Get all countries")
+            countryRepository.GetCountries()
+        with ex
+            -> logger.LogError(ex, "Error getting all countries")
+               raise (CountryControllerException("Error getting all countries", ex))
+
 
     [<HttpGet("{id}")>]
     member _.Get(id) : Country =
-        logger.LogInformation("Get country by id")
-        countryRepository.GetCountry(id) 
+        try
+            logger.LogInformation("Get country by id")
+            countryRepository.GetCountry(id) 
+        with ex
+            -> logger.LogError(ex, "Error getting country by id")
+               raise (CountryControllerException("Error getting country by id", ex))
 
     [<HttpPost>]
     member _.Post([<FromBody>] country: CountryRequest): IActionResult =
-        logger.LogInformation("Create country")
-        countryRepository.CreateCountry(country.Map()) |> ignore
-        base.Ok() :> IActionResult
+        try
+            logger.LogInformation("Create country")
+            countryRepository.CreateCountry(country.Map()) |> ignore
+            base.Ok() :> IActionResult
+        with ex
+            -> logger.LogError(ex, "Error creating country")
+               raise (CountryControllerException("Error creating country", ex))
 
     [<HttpPut>]
     member _.Put([<FromBody>] country) : IActionResult =
-        logger.LogInformation("Update country")
-        countryRepository.UpdateCountry(country) |> ignore
-        base.Ok() :> IActionResult
+        try
+            logger.LogInformation("Update country")
+            countryRepository.UpdateCountry(country) |> ignore
+            base.Ok() :> IActionResult
+        with ex
+            -> logger.LogError(ex, "Error updating country")
+               raise (CountryControllerException("Error updating country", ex))
 
     [<HttpDelete>]
     member _.Delete([<FromBody>] country) : IActionResult =
-        logger.LogInformation("Delete country")
-        countryRepository.DeleteCountry(country) |> ignore
-        base.Ok() :> IActionResult
+        try
+            logger.LogInformation("Delete country")
+            countryRepository.DeleteCountry(country) |> ignore
+            base.Ok() :> IActionResult
+        with ex
+            -> logger.LogError(ex, "Error deleting country")
+               raise (CountryControllerException("Error deleting country", ex))
